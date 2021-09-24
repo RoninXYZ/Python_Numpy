889e4475f7fe568722425aaa49c8ff1394bfd3c6
RONIN <xyzronin97@gmail.com>
2021-09-24 10:23:01 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ afa21.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+x = arr.view()
+arr[0]=42
+arr[1]=24
+print(arr)
+print(x)
---
+++ alfa25.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr = np.array([1,2,3,4],ndmin = 5)
+arr1 = np.array([[1,2,3],[1,2,3]])
+print(arr)
+print("shape of array :" , arr.shape)
+print(arr1.shape)
